dad2129e08501d27513913dc8bd181b5df249aa1
mooizahmed102 <amoiz6263@gmail.com>
2023-09-21 20:08:48 +0500

    first commit

--- views/home.php
+++ views/home.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!--====== Title ======-->
-    <title>Wonted - Book and publisher landing page</title>
+    <title>Lerric Odd</title>
     <!-- ========== Favicon Ico ========== -->
     <link rel="icon" href="assets/images/favicon.ico" />
     <link
